b785be4982943c79f1ed63dfba9975a3ad277b07
fariha-yamani14 <farihayamani14@gmail.com>
2024-08-15 22:58:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var question1 = prompt("what does html stand for?");
+if (question1 == "hyper text markup language") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question2 = prompt("What is the largest planet in our solar system?");
+if (question2 == "mars") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question3 = prompt("What is the capital city of Japan?");
+if (question3 == "tokyo") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question4 = prompt("What is the smallest planet in our solar system?");
+if (question4 == "Mercury") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question5 = prompt("Which element has the chemical symbol 'O'?");
+if (question5 == "Oxygen") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question6 = prompt("Who wrote the play Romeo and Juliet?");
+if (question6 == "William Shakespeare") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question7 = prompt(" What is the capital city of Australia?");
+if (question7 == "Canberra") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question8 = prompt(" What is the chemical symbol for Gold?");
+if (question8 == "Au") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question9 = prompt("Which country is the birthplace of the Olympic Games?");
+if (question9 == "Greece") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
+
+var question10 = prompt("What is the longest river in the world?");
+if (question10 == "Nile River") {
+    alert("correct");
+} else {
+    alert("wrong");
+}
